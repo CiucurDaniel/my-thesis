f8c6319672e00ac71bc50897fe3e5129d85f151f
Ciucur Daniel <ciucur.daniel14@gmail.com>
2023-01-30 20:37:05 +0200

    Create package for data entities

--- backend/data/user.go
+++ backend/data/user.go
@@ -1,12 +1,12 @@
-package main
+package data
 
-type user struct {
+type User struct {
 	email    string
 	password string
 	role     string // student, teacher, admin
 }
 
-var InMemDb = []user{
+var InMemDb = []User{
 	{email: "daniel.ciucur@gmail.com", password: "1234", role: "student"},
 	{email: "mike.ciucur@gmail.com", password: "1234", role: "teacher"},
 	{email: "diego.ciucur@gmail.com", password: "1234", role: "admin"},
