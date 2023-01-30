1bf18dd6f54fe6723be8071f1c7054191c51270e
Ciucur Daniel <ciucur.daniel14@gmail.com>
2023-01-30 21:48:33 +0200

    Add logout button, move toolbar on home component

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import {AuthenticationService} from "../_services/auth.service";
 export class HomeComponent implements OnInit {
 
   userRole: string | null = this.authenticationService.getRole();
+  isLoggedIn: boolean | null = this.authenticationService.isLoggedIn();
   constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
@@ -17,4 +18,7 @@ export class HomeComponent implements OnInit {
     // already been initialized
   }
 
+  public onSubmit() {
+    this.authenticationService.logout();
+  }
 }
